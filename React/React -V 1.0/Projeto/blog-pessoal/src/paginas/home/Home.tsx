40ea42c5453058823fc533b9a1be40564706f809
Victor Silva de Franca <victor.silva.franca18@gmail.com>
2022-01-13 21:45:53 -0300

    Upando React Aula 1

---
+++ React/React -V 1.0/Projeto/blog-pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://68.media.tumblr.com/c7f9c6e00d80680ec2df8eb702d88abd/tumblr_osq93vgjVR1r8l821o1_500.gif" alt="Imagem Inicial" className="img" />
+        </>
+    );
+}
+
+export default Home;
